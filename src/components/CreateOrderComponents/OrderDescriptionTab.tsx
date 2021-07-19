20fa739af95ff25038847db2a841a1b9f179d663
no-link-to-piter <no.link.to.piter@yandex.ru>
2021-07-19 16:48:48 +0300

    feature: dnd

--- src/components/CreateOrderComponents/OrderDescriptionTab.tsx
+++ src/components/CreateOrderComponents/OrderDescriptionTab.tsx
@@ -1,42 +1,90 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
+
+import {NativeTypes} from 'react-dnd-html5-backend';
+import {useDrop, DropTargetMonitor} from 'react-dnd';
+
+import { CSSTransition } from 'react-transition-group';
 
 import 'assets/sass/createOrder/orderDescription.sass';
 
-import {FilledCheckMark, FileUploadIcon} from 'shared/icons'
+import {FilledCheckMark, FileUploadIcon, CloseIcon} from 'shared/icons'
 
 const OrderDescriptionTab = () => {
 
-    const hiddenFileInput = React.useRef<HTMLInputElement>(null);
+    const hiddenFileInput = useRef<HTMLInputElement>(null);
 
     const [form, setForm] = useState({
         title: '', desc: ''
     }) 
 
-    const [files, setFiles] = useState<File[] | null>(null)   
-    const [filenames, setFilenames] = useState<String[]>([])
+    const [files, setFiles] = useState<File[]>([])   
+    const [hoveredFile, setHoveredFile] = useState<string | null>(null)
+
+    const addFiles = useCallback((chosenFiles: File[]) => {
+        const uploadedFileNames = files.length ? files.map(file => file.name) : []
+        const filesToUpload: File[] = chosenFiles.filter(file => !uploadedFileNames.includes(file.name));
+        setFiles([...filesToUpload, ...files]);
+    }, [files, setFiles])
+
+    const handleFileDrop = useCallback((item) => {
+        if (item) addFiles(item.files);
+    }, [addFiles]);
+
+    const [{canDrop, isOver}, drop] = useDrop(
+        () => ({
+            accept: [NativeTypes.FILE],
+            drop(item: { files: any[] }) {
+                if (handleFileDrop) {
+                handleFileDrop(item);
+                }
+            },
+            collect: (monitor: DropTargetMonitor) => ({
+                isOver: monitor.isOver(),
+                canDrop: monitor.canDrop(),
+            }),
+        }),
+        [handleFileDrop],
+    );
 
     const filesHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files) {
-
-            if (files) {
-                setFiles([...Array.prototype.slice.call(e.target.files), ...files])
-            } else {
-                setFiles([...Array.prototype.slice.call(e.target.files)])
-            }
-
-            let names: string[] = [];
-            if (Array.prototype.slice.call(e.target.files) && Array.prototype.slice.call(e.target.files).length) {
-                names = Array.prototype.slice.call(e.target.files).map( i => i.name)
-            }
-            setFilenames([...names])
-        }
+        if (e.target.files) addFiles(Array.prototype.slice.call(e.target.files));
     }
 
     const handleUploadClick = () => {
-        if ( hiddenFileInput.current !== null) {
-            hiddenFileInput.current.click();
+        if (hiddenFileInput.current !== null && files.length < 3) hiddenFileInput.current.click();
+    }
+
+    const handleFileHover = (fileName: string, action: string) => {
+        if (hoveredFile !== fileName && action === 'show') {
+            setHoveredFile(fileName);
+        } else if (hoveredFile === fileName && action === 'hide') {
+            setHoveredFile(null)
         }
     }
+
+    const fileNameHandler = (fileName: string) => {
+        let transformedName = fileName;
+        if (fileName.length > 36) {
+            const fileExtension = transformedName.split('.')[transformedName.split('.').length - 1];
+            const separatedFileName = transformedName.replace(`.${fileExtension}`, '');
+            transformedName = `${separatedFileName.slice(0, 33)}...${fileExtension}`;
+        }
+        return transformedName;
+    }
+
+    const removeFile = (fileName: string) => {
+        setFiles(files.filter(file => file.name !== fileName));
+    }
+
+    const isActive = canDrop && isOver;
+    let dropBlockClassName = "order-description--main--file-loader--input-place";
+    let iconColor = '#567BFF';
+
+    if (files && files.length >= 3) {
+        dropBlockClassName += ' disabled';
+        iconColor = '#D0DAFF';
+    }
+    if (isActive) dropBlockClassName += ' file-over';
 
     return (
         <div className="order-description">
@@ -73,10 +121,10 @@ const OrderDescriptionTab = () => {
                 </div>
 
                 <div className="order-description--main--file-loader">
-                    <div className='order-description--main--file-loader--input-place'>
+                    <div className={dropBlockClassName} onClick={handleUploadClick} role='presentation' ref={drop}>
 
-                        <div onClick={handleUploadClick} role='presentation' className='order-description--main--file-loader--input-place--info'>
-                            <FileUploadIcon/>
+                        <div className="order-description--main--file-loader--input-place--info">
+                            <FileUploadIcon fill={iconColor}/>
                             <h2>Загрузите файлы</h2>
                             <span>или перетяните их сюда</span>
                         </div>
@@ -91,18 +139,46 @@ const OrderDescriptionTab = () => {
                         />
                     </div>
                     {
-                        (filenames.length > 0)
-                        ?
+                        files.length > 0
+                        &&
                         <div className="order-description--main--file-loader--saved-files">
-                            {filenames.map( i => <div className="order-description--main--file-loader--saved-files--element"><div className="order-description--main--file-loader--saved-files--element--icon"><FilledCheckMark/></div>{i}</div>)}
+                            {files.map(item => (
+                                <div 
+                                    className="order-description--main--file-loader--saved-files--element"
+                                    onMouseEnter={() => handleFileHover(item.name, 'show')}
+                                    onMouseLeave={() => handleFileHover(item.name, 'hide')}
+                                >
+                                    <div className="order-description--main--file-loader--saved-files--element--icon">
+                                        <FilledCheckMark/>
+                                    </div>
+                                    {fileNameHandler(item.name)}
+                                    <CSSTransition 
+                                        timeout={100}
+                                        in={hoveredFile === item.name}
+                                        classNames={{
+                                            appear: 'order-description--main--file-loader--saved-files--element__close appear',
+                                            appearActive: 'order-description--main--file-loader--saved-files--element__close appear-active',
+                                            appearDone: 'order-description--main--file-loader--saved-files--element__close appear-done',
+                                            enter: 'order-description--main--file-loader--saved-files--element__close enter',
+                                            enterActive: 'order-description--main--file-loader--saved-files--element__close enter-active',
+                                            enterDone: 'order-description--main--file-loader--saved-files--element__close enter-done',
+                                            exit: 'order-description--main--file-loader--saved-files--element__close exit',
+                                            exitActive: 'order-description--main--file-loader--saved-files--element__close exit-active',
+                                            exitDone: 'order-description--main--file-loader--saved-files--element__close exit-done',
+                                        }}
+                                        onClick={() => removeFile(item.name)}
+                                        unmountOnExit
+                                    >
+                                        <div>
+                                            <CloseIcon/>
+                                        </div>
+                                    </CSSTransition>
+                                </div>
+                            ))}
                         </div>
-                        :
-                        false
                     }
                 </div>
             </div>
-
-
 
             <button type='button'>Продолжить</button>
         </div>
